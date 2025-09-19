09e47128b76ab5587a5524e7705e8547bacc35a7
RaeFast <202209166249@alunos.estacio.br>
2025-09-19 20:18:28 +0800

    ✨ feat(app): 重构用户脚本头部信息管理
    - 将用户脚本头部信息从 app.js 中提取到单独的 userScriptHeader.js 文件中
    - 更新 rolldown.config.js 以使用新的头部信息文件
    - 使代码结构更清晰，便于维护和管理

--- rolldown.config.js
+++ rolldown.config.js
@@ -22,18 +22,9 @@ module.exports = defineConfig({
   output: {
     file: "dist/bundle.js",
     format: "iife",
-    banner: `// ==UserScript==
-// @name        打开NestJs中文文档
-// @namespace   Violentmonkey Scripts
-// @match       https://docs.nestjs.com/*
-// @grant       none
-// @version     1.0
-// @author      Liao Brant
-// @description 从nestjs英文文档一键打开中文文档
-// @license     MIT
-// @downloadURL https://update.greasyfork.org/scripts/529353/%E6%89%93%E5%BC%80NestJs%E4%B8%AD%E6%96%87%E6%96%87%E6%A1%A3.user.js
-// @updateURL https://update.greasyfork.org/scripts/529353/%E6%89%93%E5%BC%80NestJs%E4%B8%AD%E6%96%87%E6%96%87%E6%A1%A3.meta.js
-// ==/UserScript==
-`,
+    banner: require("fs").readFileSync(
+      require("path").resolve(__dirname, "src/userScriptHeader.js"),
+      "utf8"
+    ),
   },
 });
--- src/app.js
+++ src/app.js
@@ -1,15 +1,5 @@
-// ==UserScript==
-// @name        打开NestJs中文文档
-// @namespace   Violentmonkey Scripts
-// @match       https://docs.nestjs.com/*
-// @grant       none
-// @version     1.0
-// @author      Liao Brant
-// @description 从nestjs英文文档一键打开中文文档
-// @license     MIT
-// @downloadURL https://update.greasyfork.org/scripts/529353/%E6%89%93%E5%BC%80NestJs%E4%B8%AD%E6%96%87%E6%96%87%E6%A1%A3.user.js
-// @updateURL https://update.greasyfork.org/scripts/529353/%E6%89%93%E5%BC%80NestJs%E4%B8%AD%E6%96%87%E6%96%87%E6%A1%A3.meta.js
-// ==/UserScript==
+import "../src/userScriptHeader.js";
+// ...existing code...
 
 (function () {
   "use strict";
